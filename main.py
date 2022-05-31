72af5cbae988e8bc846c3709a14a3862d4def859
Oscar <osscaaryyip@gmail.com>
2022-05-31 07:58:35 -0700

    Change Environ Key

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 ##CONNECT TO DB
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
